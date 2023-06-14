8a6c8b4bdd911145bd52829579d5ab92213b875d
Andrea Matsunaga <andreamizuno@gmail.com>
2023-06-13 23:19:29 -0300

    feat: adiciona estilo

--- src/App.jsx
+++ src/App.jsx
@@ -5,57 +5,75 @@ import { useState } from "react";
 function App() {
   const dispatch = useDispatch();
 
-  const [formData, setFormData] = useState({ nome: "", price: "" });
+  const [formData, setFormData] = useState({ name: "", price: "" });
 
   const { products } = useSelector((state) => state.products);
 
   return (
-    <>
-      {/* <h1>Hello World</h1>
-      <h2>{user}</h2>
-      <h4>{login}</h4>
-      <p>{JSON.stringify(products)}</p> */}
-      <div>
+    <div className="p-9 w-1/2">
+      <div className="flex flex-col gap-4">
         <input
+          className="p-3 rounded-lg shadow-md w-full"
           value={formData.name}
           type="text"
           placeholder="Nome do Produto"
-          onChange={(e) => setFormData({ ...formData, nome: e.target.value })}
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
         />
         <input
+          className="p-3 rounded-lg shadow-md w-full shadow-md"
           value={formData.price}
           type="text"
           placeholder="Preço"
           onChange={(e) => setFormData({ ...formData, price: e.target.value })}
         />
-        <br />
-        <button
-          onClick={() =>
-            dispatch({ type: "ADD_PRODUCT", payload: { product: formData } })
-          }
-        >
-          Salvar
-        </button>
-        <button onClick={() => dispatch({ type: "CLEAR_LIST" })}>
-          Limpar lista
-        </button>
+        <div className="flex gap-3 justify-center mb-4">
+          <button
+            className="bg-green-500 rounded-lg"
+            onClick={() =>
+              dispatch({ type: "ADD_PRODUCT", payload: { product: formData } })
+            }
+          >
+            Salvar
+          </button>
+          <button
+            className="bg-red-300 rounded-lg shadow-md"
+            onClick={() => dispatch({ type: "CLEAR_LIST" })}
+          >
+            Limpar lista
+          </button>
+        </div>
       </div>
 
-      <ul>
-        {products.map((product, index) => (
-          <>
-            <li key={index}>{JSON.stringify(product)}</li>
-            <button
-              onClick={() =>
-                dispatch({ type: "REMOVE_PRODUCT", payload: { index: index } })
-              }
+      <table className="w-full">
+        <tbody>
+          {/* <ul className="w-full"> */}
+          {products.map((product, index) => (
+            // <li key={index} className="w-full flex items-center gap-3">
+            <tr
+              key={index}
+              className="w-100 flex justify-between items-center border-y"
             >
-              Remover
-            </button>
-          </>
-        ))}
-      </ul>
-    </>
+              <td className="grow text-lg font-light pl-3">{product.name}</td>
+              <td className="grow text-lg font-light pl-3">{product.price}</td>
+              <td>
+                <button
+                  className="bg-red-400 rounded-lg"
+                  onClick={() =>
+                    dispatch({
+                      type: "REMOVE_PRODUCT",
+                      payload: { index: index },
+                    })
+                  }
+                >
+                  Remover
+                </button>
+              </td>
+            </tr>
+          ))}
+          {/* </ul> */}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
